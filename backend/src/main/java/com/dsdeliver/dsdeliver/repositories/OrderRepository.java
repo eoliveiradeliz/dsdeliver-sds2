7d4134e4c079ccb982fe41ebfbd139525d504d00
unknown <eoliveiradeliz@gmail.com>
2021-01-10 11:06:00 -0300

    GET /products

---
+++ backend/src/main/java/com/dsdeliver/dsdeliver/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.dsdeliver.dsdeliver.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dsdeliver.dsdeliver.entities.Order;
+
+public interface  OrderRepository extends JpaRepository<Order, Long>{
+	
+
+}
